301acadfc66db8e54b6acb940135bc9213ac4b33
tamim-111 <tamim144362@gmail.com>
2025-03-02 20:13:38 +0600

    Changing theme button added

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,17 @@
 document.getElementById("discoverSomethingBtn").addEventListener("click", function(){
     window.location.href = "question.html";
 });
+
+// changing the theme
+let number = 0;
+document.getElementById("themeBtn").addEventListener("click", function(){
+    number++;
+    const arrayOfColors = [
+        "Red", "Blue", "Green", "Yellow", "Purple", "Orange", "Pink", "Brown", "Black", "White",
+        "Gray", "Cyan", "Magenta", "Lime", "Maroon", "Navy", "Olive", "Teal", "Aqua", "Silver",
+        "Gold", "Beige", "Turquoise", "Lavender", "Violet", "Indigo", "Coral", "Salmon", "Crimson", "Khaki",
+        "Plum", "Orchid", "Tomato", "Azure", "Ivory", "Mint", "Peach", "Chocolate", "SkyBlue", "Sienna",
+        "Chartreuse", "SlateBlue", "DeepPink", "MediumPurple", "DarkCyan", "ForestGreen", "DodgerBlue", "HotPink", "MediumSeaGreen", "RoyalBlue"
+    ];
+    document.getElementById("body").style.backgroundColor = arrayOfColors[number];
+});
